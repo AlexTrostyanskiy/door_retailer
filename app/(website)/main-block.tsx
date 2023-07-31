dfd51778c1926f1a883906141010532138f88083
Alexander_Trostyanskiy <60698313+AlexTrostyanskiy@users.noreply.github.com>
2023-07-31 23:57:34 +0200

    Minor changes

--- app/(website)/main-block.tsx
+++ app/(website)/main-block.tsx
@@ -9,7 +9,7 @@ export function MainBlock() {
                     velit</p>
                 <button
                     className="bg-amber-300 hover:bg-amber-400 active:bg-amber-500 transition-all mt-2 p-2 px-4 rounded-md">
-                    Заказать звонок
+                    Быстрая консультация
                 </button>
             </div>
         </div>
--- app/(website)/services-block.tsx
+++ app/(website)/services-block.tsx
@@ -3,7 +3,7 @@ export function ServicesBlock({ className }) {
         <div className={className}>
             <div className="container">
                 <div className="flex flex-row gap-12 justify-center">
-                    <div className="bg-white p-8 shadow-xl rounded-md">Services</div>
+                    <div className="bg-white p-8 shadow-xl rounded-md">Services 1</div>
                     <div className="bg-white p-8 shadow-xl rounded-md">Services 2</div>
                     <div className="bg-white p-8 shadow-xl rounded-md">Services 3</div>
                     <div className="bg-white p-8 shadow-xl rounded-md">Services 4</div>
