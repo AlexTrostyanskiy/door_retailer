c4cbae598e1a0cd881fb37b743750b8c4ea38e6d
Alexander_Trostyanskiy <60698313+AlexTrostyanskiy@users.noreply.github.com>
2023-10-15 18:08:50 +0200

    catalog grid

--- app/(website)/about/page.tsx
+++ app/(website)/about/page.tsx
@@ -3,9 +3,9 @@ import { getAllAuthors, getSettings } from "@/lib/sanity/client";
 export default async function AboutPage() {
   const authors = await getAllAuthors();
   const settings = await getSettings();
-  return <div>
+  return <div className="flex justify-center">
 <p>
-  Тут текст о нас
+  Тут текст о нас Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p>
   </div>;
 }
--- app/(website)/catalog/page.tsx
+++ app/(website)/catalog/page.tsx
@@ -3,9 +3,9 @@ import { getAllAuthors, getSettings } from "@/lib/sanity/client";
 export default async function AboutPage() {
   const authors = await getAllAuthors();
   const settings = await getSettings();
-  return <div>
-<p>
-  Каталог и блок фильтров слева
-</p>
+  return <div className="grid grid-rows-3 grid-flow-col gap-4">
+<div className="bg-gray-500 row-span-6">1</div>
+<div className="bg-slate-500 col-span-3">2</div>
+<div className="bg-red-300 row-span-5 col-span-3">3</div>
   </div>;
 }
