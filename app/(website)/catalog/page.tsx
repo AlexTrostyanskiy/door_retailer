3dc4656305b5f4c7e6c23d1ff15270e071d96f34
Alexander_Trostyanskiy <60698313+AlexTrostyanskiy@users.noreply.github.com>
2023-10-16 19:29:27 +0200

    grid

--- app/(website)/catalog/page.tsx
+++ app/(website)/catalog/page.tsx
@@ -3,9 +3,9 @@ import { getAllAuthors, getSettings } from "@/lib/sanity/client";
 export default async function AboutPage() {
   const authors = await getAllAuthors();
   const settings = await getSettings();
-  return <div className="grid grid-rows-3 grid-flow-col gap-4">
-<div className="bg-gray-500 row-span-6">1</div>
-<div className="bg-slate-500 col-span-3">2</div>
-<div className="bg-red-300 row-span-5 col-span-3">3</div>
+  return <div className="grid grid-rows-3 grid-flow-col gap-2">
+    <div className="bg-gray-500 row-span-3 rounded-lg">01-фильтры</div>
+    <div className="bg-slate-500 col-span-5 rounded-lg">02-категории</div>
+    <div className="bg-red-300 row-span-2 col-span-5 rounded-lg">03-каталог</div>
   </div>;
 }
