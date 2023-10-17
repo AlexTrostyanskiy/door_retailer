eca20d13fd2b5eb3c884ff8881fce995c95d0bee
slawiko <shchaurouski.slava@gmail.com>
2023-10-17 19:41:00 +0200

    New door thumbnail

--- components/door-thumbnail.tsx
+++ components/door-thumbnail.tsx
@@ -1,18 +1,20 @@
-import { Button } from './button';
+import { Button } from "./button";
 
 interface DoorThumbnailProps {
-    title: string;
-    price: number;
-    imageSrc: string;
+  title: string;
+  price: number;
+  imageSrc: string;
 }
 
 export function DoorThumbnail(props: DoorThumbnailProps) {
-    return (
-        <div className="flex flex-col items-center shadow-xl rounded-xl p-8">
-            <h2 className="font-semibold mb-2">{props.title}</h2>
-            <img className="shadow-xl" src={props.imageSrc} alt="" />
-            <h5 className="mt-2">{props.price} р.</h5>
-            <Button>Рассчитать</Button>
-        </div>
-    )
+  return (
+    <div className="flex flex-row rounded-xl gap-4 p-8 shadow-xl">
+      <img className="max-h-64 shadow-xl" src={props.imageSrc} alt="" />
+      <div className="flex flex-col justify-start">
+        <h2 className="mb-2 font-semibold">{props.title}</h2>
+        <h5 className="mt-2">{props.price} р.</h5>
+        <Button className="mt-auto">Рассчитать</Button>
+      </div>
+    </div>
+  );
 }
