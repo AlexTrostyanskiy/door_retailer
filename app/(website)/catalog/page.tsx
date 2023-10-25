74f3431d057bb149fb1fc6c97747f965d09528ce
Alexander_Trostyanskiy <60698313+AlexTrostyanskiy@users.noreply.github.com>
2023-10-25 23:32:53 +0200

    Change the catalog view to wide

--- app/(website)/catalog/page.tsx
+++ app/(website)/catalog/page.tsx
@@ -11,16 +11,15 @@ interface Door {
 export default function Catalog() {
   const doors = getDoors();
   return (
-    // TODO: get back to height
-    <div className="flex gap-2 grow-0 mx-2 w-2/3 self-center">
-      <div className=" h-max bg-gray-300 rounded-lg basis-1/5">
+    <div className="flex gap-2 grow-0 mx-2">
+      <div className="h-max bg-gray-300 rounded-lg basis-1/4">
         <div className='bg-teal-100 rounded-lg m-2 py-1 px-2'>категории</div>
         <div className='bg-teal-100 rounded-lg m-2 py-1 px-2'>фильтры</div>
         <div className='bg-teal-100 rounded-lg m-2 py-1 px-2'>еще фильтры</div>
         <div className='bg-teal-100 rounded-lg m-2 py-1 px-2'>что-то еще</div>
         <div className='bg-teal-100 rounded-lg m-2 py-1 px-2'>тоже фильтры</div>
         </div>
-      <div className=" bg-slate-300 rounded-lg overflow-auto flex gap-12 p-12 flex-wrap place-items-center basis-4/5">
+      <div className=" bg-slate-300 rounded-lg overflow-auto flex gap-12 p-12 flex-wrap place-items-center basis-3/4">
         
           {doors.map((door) => <DoorThumbnail price={door.price} title={door.title} imageSrc={door.imageSrc} category={door.category}/>)}
         
