aa85a3a3a55bbe70f03ea9c4b77692533961d68d
Alexander_Trostyanskiy <60698313+AlexTrostyanskiy@users.noreply.github.com>
2023-11-16 23:57:50 +0100

    catalog componrnt

--- app/(website)/catalog/page.tsx
+++ app/(website)/catalog/page.tsx
@@ -1,8 +1,19 @@
+import { Catalog } from "../catalog";
 
-
-export function Catalog() {
+export function Catalog({ posts }) {
 
   return (
-  
+    <Catalog doors={posts} />
   );
+}
+
+export default async function IndexPage() {
+  const products = await getAllProducts();
+  const productsWithImage = products.map((product) => {
+    return {
+      ...product,
+      imageSrc: urlForImage(product.image).src,
+    }
+  });
+  return <Catalog posts={productsWithImage} />;
 }
