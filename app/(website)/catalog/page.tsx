e7c696b15aa57463c6240139c2638e4c78045b08
Alexander_Trostyanskiy <60698313+AlexTrostyanskiy@users.noreply.github.com>
2023-11-18 00:00:17 +0100

    catalog

--- app/(website)/catalog/page.tsx
+++ app/(website)/catalog/page.tsx
@@ -11,8 +11,7 @@ export default async function IndexPage() {
   const products = await getAllProducts();
   const productsWithImage = products.map((product) => {
     return {
-      ...product,
-      imageSrc: urlForImage(product.image).src,
+     
     }
   });
   return <Catalog posts={productsWithImage} />;
